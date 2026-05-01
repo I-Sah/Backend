dc239003ee3197df3daf6db10aac1b8cfb73e168
thephaniesissie <thephaniesissie@gmail.com>
2026-05-01 17:46:45 +0300

    my 5th commit

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -8,7 +8,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: 'http://localhost:5003/auth/google/callback', // Doit matcher la console Google
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5003/auth/google/callback', // Doit matcher la console Google
       scope: ['email', 'profile'],
     });
   }
