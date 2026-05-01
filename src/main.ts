1f16640765832d21b7defdc5cd9c2192d506457d
thephaniesissie <thephaniesissie@gmail.com>
2026-05-01 16:17:28 +0300

    my 4th commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Auth API')
     .setDescription('API d\'authentification avec JWT, Google et Facebook')
